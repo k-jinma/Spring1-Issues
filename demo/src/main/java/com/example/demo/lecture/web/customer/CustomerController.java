03dae82602e7b52b37450ba0dd389ba16fe6b4e2
k-jinma <jinmakazuhiro@outlook.com>
2024-07-22 01:01:00 +0900

    不要なimportを削除

--- demo/src/main/java/com/example/demo/lecture/web/customer/CustomerController.java
+++ demo/src/main/java/com/example/demo/lecture/web/customer/CustomerController.java
@@ -12,7 +12,6 @@ import com.example.demo.lecture.service.CustomerService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
